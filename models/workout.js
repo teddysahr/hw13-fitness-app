2bb63aadc3839df17463e83b06cbb8b0ae29c268
Teddy Sahr <tedward1218@gmail.com>
2021-12-07 23:02:09 -0500

    created workout model and added index

--- models/workout.js
+++ models/workout.js
@@ -2,19 +2,23 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  name: {
+const WorkoutSchema = new Schema({
+  day: {
     type: String,
     unique: true,
   },
-  notes: [
+  exercises: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "Note",
+      type: { type: String },
+      name: { type: String },
+      duration: { type: Number },
+      weight: { type: Number },
+      reps: { type: Number },
+      sets: { type: Number },
     },
   ],
 });
 
-const User = mongoose.model("User", UserSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
-module.exports = User;
+module.exports = Workout;
